f981452671337170d0825ff8ab18c15794d96080
Christopher Meyer <chris@schwiiz.org>
2020-06-05 17:15:50 +0200

    updates

--- src/handlers/members.js
+++ src/handlers/members.js
@@ -3,20 +3,19 @@ module.exports = session => ({
 	USER: 0,
 	GROUP: 1,
 
-	// https://developer.opentext.com/awd/resources/apis/cs-rest-api-for-cs-16-s#!/members/SearchMember_get_1
-	userQuery(query, options = {}) {
+	userQuery(query, options = {}, version='v2') {
 		const params = {
 			limit: 20,
-			where_type: [this.USER, this.GROUP],
+			where_type: JSON.stringify([this.USER, this.GROUP]),
 			query,
 			...options
 		}
 
-		return session.get(`api/v2/members`, {params})
+		return session.get(`api/${version}/members`, {params})
 	},
 
-	member(id) {
-		return session.get(`api/v2/members/${id}`)
+	member(id, version='v2') {
+		return session.get(`api/${version}/members/${id}`)
 	}
 
 })
