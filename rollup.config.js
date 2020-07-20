7a6987fdfec53000d86d7dca8c39c27f2342a40c
Christopher Meyer <chris@schwiiz.org>
2020-07-20 09:02:17 +0200

    1.0.11

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import pkg from './package.json'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import replace from '@rollup/plugin-replace'
 
